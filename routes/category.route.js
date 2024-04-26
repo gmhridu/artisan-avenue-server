d65f45f88062332221540ee17d61199ef0f99f4b
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-04-26 09:43:38 +0600

    Integrated routes in categoryRouter

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const categoryRouter = express.Router();
+const { addProduct, getAllCategories, getSingleCategory, updateCategory, deleteCategory } = require('../controllers/category.controller');
+
+// create categories
+categoryRouter.post('/', addProduct);
+
+// get all categories
+categoryRouter.get('/', getAllCategories);
+
+// get single categories by id
+categoryRouter.get('/:id', getSingleCategory);
+
+// update categories by id
+categoryRouter.put('/:id', updateCategory);
+
+// delete categories by id
+categoryRouter.delete('/:id', deleteCategory);
 
 
 module.exports = categoryRouter;
